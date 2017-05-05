e06a2fda80aad4e7268bcb64f515127753c5adc8
wm <satileW@users.noreply.github.com>
2017-05-05 11:59:37 -0700

    Add files via upload

---
+++ src/main/java/rest/RestApplication.java
@@ -0,0 +1,54 @@
+package rest;
+import java.util.Set;
+
+import module.*;
+import repo.ApplicationRepo;
+
+public class RestApplication {
+	private ApplicationRepo applicationRepo;
+	Application createApplication(JobSeeker jobSeeker,String sEmail,String sFirstName,String sLastName, Position position, String resumeURL){
+		/*para: 
+		sID:   job seeker id
+		sEmail: email same with sID
+		sFirstName: first name same with sID
+		sLastName: first name same with sID
+		pID:  job position id
+		*/
+		//judgement of application: 1.A user cannot have more than 5 pending applications. 2.A user cannot apply for the same position again if the previous application is not in a terminal state
+		Application application = new Application();
+		return applicationRepo.save(application);// with initial status(pending) + new generate aID.
+	}
+	Application getApplication(Long aID){
+		
+		Application application = applicationRepo.findOne(aID);
+		return application;
+	}
+	Application updateApplication(Long aID, String status){
+	//to change application status
+		Application application = applicationRepo.findOne(aID);
+		application.setStatus(status);
+		return application;
+	//email update
+	}
+	void updateApplications(Set<Long> aIDs){
+		//status==cancel
+		//seeker cancel applications,Set<Application>  user can cancel one or more applications
+		for(Long aID : aIDs){
+			Application application = applicationRepo.findOne(aID);
+			application.setStatus("Cancelled");
+			applicationRepo.save(application);
+			//TODO email update the application's jobseeker
+		}
+		
+	}
+	Boolean deleteApplication(Long aID){
+		Application application = applicationRepo.findOne(aID);
+		if(application!=null){
+			applicationRepo.delete(application);
+			return true;
+		}
+		return false;
+	}
+
+
+}
---
+++ src/main/java/rest/RestPosition.java
@@ -0,0 +1,63 @@
+package rest;
+
+import repo.PositionRepo;
+import module.Position;
+public class RestPosition {
+	private PositionRepo positionRepo;
+	Position createPostion(String pTitle,String pDesciption,String responsibility,String officeLocation,Long salary) {
+		// with initial status + new generate pID
+		Position position = new Position();
+		position.setDescription(pDesciption);
+		position.setResponsibility(responsibility);
+		position.setOfficeLocation(officeLocation);
+		position.setSalary(salary);
+		position.setStatus("pending");
+		return positionRepo.save(position);
+	}
+	Position getPosition(Long pID){
+		return positionRepo.findOne(pID);
+	}
+	Position updatePosition(Long pID,String pTitle,String pDesciption,String responsibility,String officeLocation,Long salary,String status){
+		//Notification all the seeker and Same return with create.
+		Position position = positionRepo.findOne(pID);
+		if(position!=null){
+			if(pTitle!=null){
+				position.setTitle(pTitle);
+			}
+			if(pDesciption!=null){
+				position.setDescription(pDesciption);
+			}
+			if(officeLocation!=null){
+				position.setOfficeLocation(officeLocation);
+			}
+			if(salary!=null){
+				position.setSalary(salary);
+			}
+			if(status!=null){
+				position.setStatus(status);
+			}
+			positionRepo.save(position);
+		}
+		return position;
+	}
+	boolean deletePosition(Long pID){ 
+		Position position = positionRepo.findOne(pID);
+		if(position!=null){
+			try{
+				positionRepo.delete(position);
+			}catch(Exception e){
+				// fail delete, MySql throw exception
+				return false;
+			}
+			// success delete: true
+			return true;
+		}
+	
+		//not find pID or fail delete : false
+		return false;
+	}
+	Void notificationSeeker(){
+		//listen function, listen to the changes of itself and notify its seekerSet
+	}
+
+}
