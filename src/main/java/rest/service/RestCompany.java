dece78fcd01903f85254159e98c91b0395faa655
GCystem <georgey.chen36@gmail.com>
2017-05-08 22:03:20 -0700

    Company Repo

--- src/main/java/rest/service/RestCompany.java
+++ src/main/java/rest/service/RestCompany.java
@@ -1,5 +1,42 @@
 package rest.service;
 
-public class RestCompany {
+import rest.repo.ApplicationRepo;
+import rest.repo.CompanyRepo;
+import rest.repo.JobSeekerRepo;
+import rest.repo.PositionRepo;
 
+import rest.module.*;
+
+public class RestCompany {
+	private JobSeekerRepo repo_jobseeker;
+	private CompanyRepo repo_company;
+	private ApplicationRepo repo_application;
+	private PositionRepo repo_position;
+	
+	public RestCompany(JobSeekerRepo repo_jobseeker, CompanyRepo repo_company, ApplicationRepo repo_application, PositionRepo repo_position){
+		this.repo_jobseeker = repo_jobseeker;
+		this.repo_company = repo_company;
+		this.repo_application = repo_application;
+		this.repo_position = repo_position;
+	} 	
+	
+	public String create_company(){
+		
+	}
+	
+	public String read_company(){
+		
+	}
+	
+	public String update_company(){
+		
+	}
+	
+	public delete_company(){
+		
+	}
+	
+	
+	
+	
 }
